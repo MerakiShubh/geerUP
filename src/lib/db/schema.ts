c9a074880a3a4f8a196be8c4efd13461200b82e2
MerakiShubh <shubhamt.champ@gmail.com>
2024-07-22 12:39:53 +0530

    add create prodcut endpoint

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,5 +1,12 @@
 import { sql } from "drizzle-orm";
-import { pgTable, serial, varchar, text, timestamp } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  varchar,
+  text,
+  timestamp,
+  integer,
+} from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
@@ -10,6 +17,16 @@ export const users = pgTable("users", {
   externalId: varchar("external_id", { length: 100 }).notNull(),
   image: text("image"),
   role: varchar("role", { length: 12 }).notNull().default("customer"),
+  updatedAt: timestamp("updated_at").default(sql`CURRENT_TIMESTAMP`),
+  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
+});
+
+export const products = pgTable("products", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 100 }).notNull(),
+  image: text("image"),
+  description: text("description"),
+  price: integer("price").notNull(),
   updatedAt: timestamp("updated_at").default(sql`CURRENT_TIMESTAMP`),
   createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
 });
