8c9618ec0fd5b7de8dde4b868268d564d6f49f3a
MerakiShubh <shubhamt.champ@gmail.com>
2024-08-04 20:13:56 +0530

    add product form

---
+++ src/app/admin/products/product-sheet.tsx
@@ -0,0 +1,49 @@
+import React, { use } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import CreateProductForm from "./create-product-form";
+import { FormValues } from "./create-product-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createProduct } from "@/http/api";
+import { useNewProduct } from "@/store/product/product-store";
+const ProductSheet = () => {
+  const { isOpen, onClose } = useNewProduct();
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationKey: ["create-product"],
+    mutationFn: (data: FormData) => createProduct(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["products"] });
+      alert("Product Created");
+    },
+  });
+  const onSubmit = (values: FormValues) => {
+    const formdata = new FormData();
+    formdata.append("name", values.name);
+    String(values.price);
+    formdata.append("description", values.description);
+    formdata.append("price", String(values.price));
+    formdata.append("image", (values.image as FileList)[0]);
+
+    mutate(formdata);
+  };
+
+  return (
+    <Sheet open={isOpen} onOpenChange={onClose}>
+      <SheetContent className="min-w-[28rem] space-y-4">
+        <SheetHeader>
+          <SheetTitle>Create Product</SheetTitle>
+          <SheetDescription>Create a new product</SheetDescription>
+        </SheetHeader>
+        <CreateProductForm onSubmit={onSubmit} />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default ProductSheet;
