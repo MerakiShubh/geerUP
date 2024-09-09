032e08e8ab9e962fcca01f53cf9e32c0ae578bea
MerakiShubh <shubhamt.champ@gmail.com>
2024-09-10 00:34:40 +0530

    add delivery-persons

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,5 +1,5 @@
-import { Warehouse } from "@/types";
 import { api } from "./client";
+import { DeliveryPerson, Warehouse } from "@/types";
 
 export const getAllProducts = async () => {
   const response = await api.get("/products");
@@ -22,5 +22,15 @@ export const getAllWarehouses = async () => {
 
 export const createWarehouse = async (data: Warehouse) => {
   const response = await api.post("/warehouses", data);
+  return response.data;
+};
+
+export const getAllDeliveryPersons = async () => {
+  const response = await api.get("/delivery-persons");
+  return await response.data;
+};
+
+export const createDeliveryPerson = async (data: DeliveryPerson) => {
+  const response = await api.post("/delivery-persons", data);
   return response.data;
 };
